193642ad3ee718d3cb7877e023e4095992652731
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-10-06 04:45:55 +0200

    feat: add code display

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -17,6 +17,27 @@ function generateWithVariables(element) {
   element.innerText = output;
 }
 
+const codeSnippetWithVariables = `
+function generateWithVariables(element) {<br /> &nbsp;&nbsp;
+  let output = ''; <br /> &nbsp;&nbsp;
+  const numLines = 20; <br /><br /> &nbsp;&nbsp;
+
+  for (let i = 1; i <= numLines; i++) { <br /> &nbsp;&nbsp;
+    if (i % 2 === 0 && i % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+      output += \`\${i}. bitsbots\n\`;
+    } else if (i % 2 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+      output += \`\${i}. bits\n\`;
+    } else if (i % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+      output += \`\${i}. bots\n\`;
+    } else { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+      output += \`\${i}. 0\n\`;
+    } <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+  } <br /><br /> &nbsp;&nbsp;
+
+  element.innerText = output;<br />
+}
+`;
+
 function generateWithoutVariables(element) {
   const outputLines = new Array(20)
     .fill(0)
@@ -30,6 +51,22 @@ function generateWithoutVariables(element) {
 
   element.innerText = outputLines;
 }
+
+const codeSnippetWithoutVariables = `
+function generateWithoutVariables(element) { <br /> &nbsp;&nbsp;
+  const outputLines = new Array(20) <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    .fill(0) <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    .map((_, i) => { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      if ((i + 1) % 6 === 0) return \`\${i + 1}. bitsbots\`; <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      if ((i + 1) % 2 === 0) return \`\${i + 1}. bits\`; <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      if ((i + 1) % 3 === 0) return \`\${i + 1}. bots\`; <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      return \`\${i + 1}. 0\`; <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    }) <br /> &nbsp;&nbsp&nbsp;&nbsp;
+    .join('\n'); <br /><br />&nbsp;&nbsp;
+
+  element.innerText = outputLines;<br />
+}
+`;
 
 function generateUsingArray(element) {
   const outputLines = [];
@@ -50,6 +87,27 @@ function generateUsingArray(element) {
   element.innerText = outputText;
 }
 
+const codeSnippetUsingArray = `
+function generateUsingArray(element) {<br /> &nbsp;&nbsp;
+  const outputLines = [];<br /><br/> &nbsp;&nbsp;
+
+  for (let i = 1; i <= 20; i++) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    if (i % 6 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bitsbots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 2 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bits\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. 0\`);<br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } <br /> &nbsp;&nbsp;
+  } <br /><br /> &nbsp;&nbsp;
+
+  const outputText = outputLines.join('\n'); <br /> &nbsp;&nbsp;
+  element.innerText = outputText; <br />
+}
+`;
+
 function generateUsingForEach(element) {
   const outputLines = [];
 
@@ -68,6 +126,27 @@ function generateUsingForEach(element) {
   const outputText = outputLines.join('\n');
   element.innerText = outputText;
 }
+
+const codeSnippetUsingForEach = `
+function generateUsingForEach(element) { <br /> &nbsp;&nbsp;
+  const outputLines = []; <br /><br /> &nbsp;&nbsp;
+
+  Array.from({ length: 20 }).forEach((_, i) => { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    if ((i + 1) % 6 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i + 1}. bitsbots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if ((i + 1) % 2 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i + 1}. bits\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if ((i + 1) % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i + 1}. bots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i + 1}. 0\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } <br /> &nbsp;&nbsp;
+  }); <br /><br /> &nbsp;&nbsp;
+
+  const outputText = outputLines.join('\n'); <br /> &nbsp;&nbsp;
+  element.innerText = outputText; <br />
+}
+`;
 
 function generateUsingWhileLoop(element) {
   const outputLines = [];
@@ -89,6 +168,29 @@ function generateUsingWhileLoop(element) {
   const outputText = outputLines.join('\n');
   element.innerText = outputText;
 }
+
+const codeSnippetUsingWhileLoop = `
+function generateUsingWhileLoop(element) { <br /> &nbsp;&nbsp;
+  const outputLines = []; <br /> &nbsp;&nbsp;
+  let i = 1; <br /><br /> &nbsp;&nbsp;
+
+  while (i <= 20) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    if (i % 6 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bitsbots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 2 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bits\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. 0\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    i++; <br /> &nbsp;&nbsp;
+  } <br /><br /> &nbsp;&nbsp;
+
+  const outputText = outputLines.join('\n'); <br /> &nbsp;&nbsp;
+  element.innerText = outputText; <br />
+}
+`;
 
 function generateUsingRecursion(element) {
   let outputLines = [];
@@ -116,14 +218,41 @@ function generateUsingRecursion(element) {
   recursiveRun(element);
 }
 
-generateWithVariables(document.getElementById('generate-with-variables'));
-generateWithoutVariables(document.getElementById('generate-without-variables'));
-generateUsingArray(document.getElementById('generate-using-array'));
-generateUsingArray(document.getElementById('generate-using-for-each'));
-generateUsingRecursion(document.getElementById('generate-using-while-loop'));
-generateUsingRecursion(document.getElementById('generate-using-recursion'));
+const codeSnippetUsingRecursion = `
+function generateUsingRecursion(element) { <br /> &nbsp;&nbsp;
+  let outputLines = []; <br /><br /> &nbsp;&nbsp;
 
-function handleMenu(menuItems, cards, activateCard) {
+  function recursiveRun(element, i = 1) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    if (i > 20) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      const outputText = outputLines.join('\n'); <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      element.innerText = outputText; <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      return; <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    } <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+
+    if (i % 6 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bitsbots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 2 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bits\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else if (i % 3 === 0) { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. bots\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } else { <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      outputLines.push(\`\${i}. 0\`); <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+    } <br /><br />  &nbsp;&nbsp;&nbsp;&nbsp;
+
+    recursiveRun(element, i + 1); <br />  &nbsp;&nbsp;
+  } <br /><br />  &nbsp;&nbsp;
+
+  recursiveRun(element); <br />
+}
+`;
+
+const outputElement = document.getElementById('output');
+
+function updateCodeBlock(codeSnippet) {
+  document.getElementById('code').innerHTML = codeSnippet;
+}
+
+function handleMenu(menuItems) {
   menuItems.forEach(menuItem => {
     menuItem.addEventListener('click', () => {
       menuItems.forEach(menuItem => {
@@ -131,34 +260,43 @@ function handleMenu(menuItems, cards, activateCard) {
       });
       menuItem.classList.add('active');
 
-      cards.forEach(card => {
-        card.classList.remove('active');
-      });
-      if (menuItem.textContent === 'with variables')
-        activateCard('generate-with-variables');
+      if (menuItem.textContent === 'with variables') {
+        generateWithVariables(outputElement);
+        updateCodeBlock(codeSnippetWithVariables);
+      }
 
-      if (menuItem.textContent === 'without variables')
-        activateCard('generate-without-variables');
+      if (menuItem.textContent === 'without variables') {
+        generateWithoutVariables(outputElement);
+        updateCodeBlock(codeSnippetWithoutVariables);
+      }
 
-      if (menuItem.textContent === 'using array')
-        activateCard('generate-using-array');
+      if (menuItem.textContent === 'using array') {
+        generateUsingArray(outputElement);
+        updateCodeBlock(codeSnippetUsingArray);
+      }
 
-      if (menuItem.textContent === 'using for each loop')
-        activateCard('generate-using-for-each');
+      if (menuItem.textContent === 'using for each loop') {
+        generateUsingForEach(outputElement);
+        updateCodeBlock(codeSnippetUsingForEach);
+      }
 
-      if (menuItem.textContent === 'using while loop')
-        activateCard('generate-using-while-loop');
+      if (menuItem.textContent === 'using while loop') {
+        generateUsingWhileLoop(outputElement);
+        updateCodeBlock(codeSnippetUsingWhileLoop);
+      }
 
-      if (menuItem.textContent === 'using recursion')
-        activateCard('generate-using-recursion');
+      if (menuItem.textContent === 'using recursion') {
+        generateUsingRecursion(outputElement);
+        updateCodeBlock(codeSnippetUsingRecursion);
+      }
     });
   });
 }
 
-function activateCard(id) {
-  document.getElementById(id).parentElement.classList.add('active');
+function onLoad() {
+  generateWithVariables(outputElement);
+  updateCodeBlock(codeSnippetWithVariables);
+  handleMenu(document.querySelectorAll('.menu .menu-item'));
 }
-const menuItems = document.querySelectorAll('.menu .menu-item');
-const cards = document.querySelectorAll('.cards .card');
 
-handleMenu(menuItems, cards, activateCard);
+window.addEventListener('load', onLoad);
